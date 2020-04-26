34f31a52a2b19a9a2e45f41bfd6ab1083ac2b830
Badri Narayanan <programmingwithbadri@outlook.com>
2020-04-26 11:45:11 +0530

    Add mongoose connection

--- socketMain.js
+++ socketMain.js
@@ -1,30 +1,44 @@
-function socketMain(io, socket){
-    console.log("A socket connectd!", socket.id)
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://badri:12td0426@ds237357.mlab.com:37357/heroku_xlcgbdkk', {useNewUrlParser: true});
+const Machine = require('./models/Machine');
 
-    socket.on('clientAuth',(key)=>{
-        if(key === '5t78yuhgirekjaht32i3'){
-            // valid nodeClient
-            socket.join('clients');
-        }else if(key === 'uihjt3refvdsadf'){
-            // valid ui client has joined
-            socket.join('ui');
-            console.log("A react client has joined!");
-            Machine.find({}, (err,docs)=>{
-                docs.forEach((aMachine)=>{
-                    // on load, assume that all machines are offline
-                    aMachine.isActive = false;
-                    io.to('ui').emit('data',aMachine);
-                })
-            })
-        }else{
-            // an invalid client has joined. Goodbye
-            socket.disconnect(true);
-        }
-    })
+function socketMain(io, socket) {
+  console.log("A socket connectd!", socket.id);
 
-    socket.on('perfData',(data)=>{
-        console.log(data)
-    });
+  socket.on("clientAuth", (key) => {
+    if (key === "5t78yuhgirekjaht32i3") {
+      // valid nodeClient
+      socket.join("clients");
+    } else if (key === "uihjt3refvdsadf") {
+      // valid ui client has joined
+      socket.join("ui");
+      console.log("A react client has joined!");
+      Machine.find({}, (err, docs) => {
+        docs.forEach((aMachine) => {
+          // on load, assume that all machines are offline
+          aMachine.isActive = false;
+          io.to("ui").emit("data", aMachine);
+        });
+      });
+    } else {
+      // an invalid client has joined. Goodbye
+      socket.disconnect(true);
+    }
+  });
+
+  // a machine has connected, check to see if it's new.
+  // if it is, add it!
+  socket.on("initPerfData", async (data) => {
+    // update our socket connect function scoped variable
+    macA = data.macA;
+    // now go check mongo!
+    const mongooseResponse = await checkAndAdd(data);
+    console.log(mongooseResponse);
+  });
+
+  socket.on("perfData", (data) => {
+    console.log(data);
+  });
 }
 
 module.exports = socketMain;
